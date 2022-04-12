e87ecdea98b1aab7ca8f67e1bfe51879bd92c7db
jackransomlovell <jack.lovell@colorado.edu>
2022-04-12 13:57:29 -0400

    adding canny labels to script

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from skimage.measure import label
 from skimage.segmentation import watershed
 from scipy import ndimage as ndi
 from skimage.filters import median
+from skimage import feature
 from matplotlib.colors import ListedColormap
 import numpy as np
 import argparse
@@ -18,9 +19,15 @@ def main():
                         help='Path to imgs')
     args = parser.parse_args()
     img_path = args.img_path
-    #creating a collection with the available images
     col = imread_collection(img_path)
+    #original fig
+    og_fig = imshow_collection(col)
+    og_fig.savefig('og_fig')
+    #conver to grayscale
     gray_col = rgb2gray(col)
+    #save grayscale
+    gray_fig = imshow_collection(gray_col)
+    gray_fig.savefig('gray_fig')
     #get avg otsu's thrsh on collection
     avg_thr = threshold_otsu(gray_col)
     #apply threshold to images
@@ -37,6 +44,21 @@ def main():
     np_avg_thr = np.array(avg_thr_imgs)
     #get difference b/w each
     diff_imgs = avg_thr_imgs ^ np_indiv_thr
+    
+    #get canny to draw circle
+    canny_avg = np.zeros((np_avg_thr.shape))
+    canny_indiv = np.zeros((np_indiv_thr.shape))
+    for ind, img in enumerate(np_avg_thr):
+        canny_avg[ind,] = img    
+        canny_indiv[ind,] = np_indiv_thr[ind,]
+    
+    # get labels
+    canny_avg_label, _ = ndi.label(canny_avg)
+    canny_indiv_label, _ = ndi.label(canny_indiv)
+    #now overlay
+    canny_avg_label_overlay = label2rgb(canny_avg_label, image=np_avg_thr, bg_label=0)
+    canny_indiv_label_overlay = label2rgb(canny_indiv_label, image=np_indiv_thr, bg_label=0)
+    
     #save each image collection
     avg_fig = imshow_collection(avg_thr_imgs)
     avg_fig.savefig('avg_thr_fig')
@@ -64,10 +86,15 @@ def main():
     
     # Use ListedColormap to create a random colormap to
     # help visualize the results
+    cmap = ListedColormap(np.random.rand(256,3))
     avg_fig = imshow_collection(avg_labels)
-    avg_fig.savefig('avg_inst_fig')
+    avg_fig.savefig('avg_inst_fig', cmap=cmap)
     indiv_fig = imshow_collection(indiv_labels)
-    indiv_fig.savefig('indiv_inst_fig')
+    indiv_fig.savefig('indiv_inst_fig', cmap=cmap)
+    
+    ## now compute contours of each
+    
+    
     
 if __name__ == '__main__':
     main()
